957e6e09ce13b9a90bbf45b8d559f731e1623775
EdisonStive <edison.gonzalez@uniminuto.edu.co>
2024-05-08 12:07:33 -0500

    Interfaz Login
    Interfaz login

--- src/login.java
+++ src/login.java
@@ -1,8 +1,11 @@
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class login extends JPanel {
-	private JTextField textField;
+	private JTextField textFieldUsuario;
+	private JTextField textFieldContraseña;
 
 	/**
 	 * Create the panel.
@@ -10,10 +13,24 @@ public class login extends JPanel {
 	public login() {
 		setLayout(null);
 		
-		textField = new JTextField();
-		textField.setBounds(25, 31, 86, 20);
-		add(textField);
-		textField.setColumns(10);
+		textFieldUsuario = new JTextField();
+		textFieldUsuario.setBounds(87, 33, 123, 20);
+		add(textFieldUsuario);
+		textFieldUsuario.setColumns(10);
+		
+		JLabel lblUsuario = new JLabel("Usuario: ");
+		lblUsuario.setBounds(23, 33, 54, 20);
+		add(lblUsuario);
+		
+		JLabel lblContraseña = new JLabel("Contraseña: ");
+		lblContraseña.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblContraseña.setBounds(10, 75, 74, 20);
+		add(lblContraseña);
+		
+		textFieldContraseña = new JTextField();
+		textFieldContraseña.setColumns(10);
+		textFieldContraseña.setBounds(87, 75, 123, 20);
+		add(textFieldContraseña);
 
 	}
 }
